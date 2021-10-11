12b2b4ed3054437635c29ed996eefdd2317d92bf
Ruslan Khamzin <hamzin362@gmail.com>
2021-10-11 15:26:26 +0400

    Debug

--- app/src/main/java/ru/startandroid/develop/testhtc/MainActivity.java
+++ app/src/main/java/ru/startandroid/develop/testhtc/MainActivity.java
@@ -4,27 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.LayoutInflater;
-import android.widget.Button;
 import android.widget.TextView;
-
-import org.json.JSONArray;
 import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
-import ru.startandroid.develop.testhtc.utils.NetworkUtils;
 
 public class MainActivity extends AppCompatActivity {
     RecyclerView recyclerView;

ea0ada741944142500738233d05b24551dd75424
Ruslan Khamzin <hamzin362@gmail.com>
2021-10-11 18:48:47 +0400

    debug

--- app/src/main/java/ru/startandroid/develop/testhtc/utils/NetworkUtils.java
+++ app/src/main/java/ru/startandroid/develop/testhtc/utils/NetworkUtils.java
@@ -8,14 +8,14 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class NetworkUtils {
-    public static String getResponseFromURL(URL url) throws IOException{
+    public static String getResponseFromURL(URL url) throws IOException {
 
         HttpURLConnection request = (HttpURLConnection) url.openConnection();
         BufferedReader jr = new BufferedReader(new InputStreamReader((InputStream) request.getContent()));
-        StringBuilder sb= new StringBuilder();
-        while(true) {
+        StringBuilder sb = new StringBuilder();
+        while (true) {
             String line = jr.readLine();
-            if(line==null){
+            if (line == null) {
                 break;
             }
             sb.append(line);
